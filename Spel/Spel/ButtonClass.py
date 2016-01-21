f9501dcf73159a2596f1dce40cc740b144cde1e2
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2016-01-21 13:48:22 +0100

    Added a button class that allows you to create a button with text anywhere. Includes button press detection.

---
+++ Spel/Spel/ButtonClass.py
@@ -0,0 +1,51 @@
+import pygame
+
+class Button():
+    def __init__(self, text, font_color, font = None, font_size = 10, bgcolor = None, texture = None, height = None, width = None):
+        self.text = text
+        self.font_color = font_color
+
+        self.height = height    #height and width of box of button to draw
+        self.width = width
+        
+        self.bgcolor = bgcolor  #color of button background (if no texture)
+        self.font = pygame.font.SysFont(font, font_size)
+        self.texture = texture  #texture for background button
+        
+        self.texture_dim = None
+        self.lastrender = None
+
+    def draw(self, posx, posy, screen):
+        if self.height != None and self.width != None:          #If there is a height and witdh set (no texture case)
+            pygame.draw.rect(screen, self.bgcolor, (posx,posy,self.width,self.height))
+            textsize = self.font.size(self.text)
+            text_width_offset = (self.width - textsize[0])/2
+            text_height_offset = (self.height - textsize[1])/2
+            textrender = self.font.render(self.text, 1, self.font_color)
+            screen.blit(textrender, (posx + text_width_offset, posy + text_height_offset))
+
+
+        elif self.texture != None:                              #if there is a texture set
+            texture = pygame.image.load(self.texture).convert()
+            screen.blit(texture, (posx, posy))
+            sizeTexture = texture.get_rect().size
+            self.texture_dim = sizeTexture
+            textsize = self.font.size(self.text)
+            text_width_offset = (sizeTexture[0] - textsize[0])/2
+            text_height_offset = (sizeTexture[1] - textsize[1])/2
+            textrender = self.font.render(self.text, 1, self.font_color)
+            screen.blit(textrender, (posx + text_width_offset, posy + text_height_offset))
+
+        self.lastrender = (posx, posy)      #Saves the position of the last time it was rendered
+
+    def pressed(self):
+        mousepos = pygame.mouse.get_pos()
+        if mousepos[0] >= self.lastrender[0] and mousepos[1] >= self.lastrender[1]:
+            if self.texture_dim != None:
+                if mousepos[0] <= self.lastrender[0] + self.texture_dim[0] and mousepos[1] <= self.lastrender[1] + self.texture_dim[1]:
+                    return True
+
+            else:
+                if mousepos[0] <= self.lastrender[0] + self.width and mousepos[1] <= self.lastrender[1] + self.height:
+                    return True
+        return False
--- Spel/Spel/Spel.py
+++ Spel/Spel/Spel.py
@@ -4,12 +4,16 @@ import random
 from pygame.locals import *
 from Player import *
 from Tile import *
+from ButtonClass import *
 
 gamename = "Frequency"
 window_height = 900
-window_width = 900
+window_width = 1300
 
-fps = 30
+Gameboard_offsetx = 125
+Gameboard_offsety = 0
+
+fps = 60
 fpsTime = pygame.time.Clock()
 
 pygame.init()
@@ -43,7 +47,7 @@ def drawitems():
     for x in range(18):
         for y in range(18):
             if mapArray[x][y].owner != None:
-                setDisplay.blit(Baseimg, (x*50+1, y*50+1))
+                setDisplay.blit(Baseimg, (x*50+1+Gameboard_offsetx, y*50+1+Gameboard_offsety))
 
 
 
@@ -58,32 +62,32 @@ def drawboard():
     for x in range(18):
         for y in range(18):
             if mapArray[x][y].biome == "s":
-                  setDisplay.blit(swampImg, (x*50, y*50))
+                  setDisplay.blit(swampImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
             elif mapArray[x][y].biome == "i":
-                  setDisplay.blit(iceImg, (x*50, y*50))
+                  setDisplay.blit(iceImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
             elif mapArray[x][y].biome == "d":
-                  setDisplay.blit(desertImg, (x*50, y*50))
+                  setDisplay.blit(desertImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
             elif mapArray[x][y].biome == "f":
-                  setDisplay.blit(forestImg, (x*50, y*50))
+                  setDisplay.blit(forestImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
             elif mapArray[x][y].biome == "w":
-                  setDisplay.blit(waterImg, (x*50, y*50))
+                  setDisplay.blit(waterImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
             elif mapArray[x][y].biome == "g":
-                  setDisplay.blit(goldImg, (x*50, y*50))
+                  setDisplay.blit(goldImg, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
     
     img = pygame.image.load("textures/black_border.png").convert()
     img.set_colorkey((255,0,255))
     for x in range(18):
         for y in range(18):
-            setDisplay.blit(img, (x*50, y*50))
+            setDisplay.blit(img, (x*50+Gameboard_offsetx, y*50+Gameboard_offsety))
 
 def drawMouseHover():
     x = pygame.mouse.get_pos()
-    posx = x[0] - x[0]%50
-    posy = x[1] - x[1]%50
-    
-    img = pygame.image.load("textures/white_border.png").convert()
-    img.set_colorkey((255,0,255))
-    setDisplay.blit(img, (posx, posy))
+    posx = (x[0] - Gameboard_offsetx) - (x[0] - Gameboard_offsetx)%50 + Gameboard_offsetx
+    posy = (x[1] - Gameboard_offsety) - (x[1] - Gameboard_offsety)%50 + Gameboard_offsety
+    if posx >= Gameboard_offsetx and posx < Gameboard_offsetx + 900 and posy >= Gameboard_offsety and posy < Gameboard_offsety + 900:
+        img = pygame.image.load("textures/white_border.png").convert()
+        img.set_colorkey((255,0,255))
+        setDisplay.blit(img, (posx, posy))
     
 def PlayerCreation(Pamount):
     playerlist = []
@@ -119,11 +123,30 @@ while True:
     drawboard()
     drawitems()
     drawMouseHover()
-    
+
     for event in pygame.event.get():
+        #print(event)
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == KEYDOWN:
+            print("keydown")
+        if event.type == MOUSEBUTTONDOWN:
+            stateMouse = pygame.mouse.get_pressed()
+            if stateMouse[0] == 1:
+                if lol.pressed():
+                    print("gold!")
+                if kek.pressed():
+                    print("Test!")
+                
+            
+
+
+
+    lol = Button("Gold", (0,255,255), font_size = 30, texture = "textures/gold.png")
+    kek = Button("test", (255,0,255), font_size = 232, height = 523, width = 800, bgcolor = (200,65,45))
+    kek.draw(52+Gameboard_offsetx,96+Gameboard_offsety, setDisplay)
+    lol.draw(420+Gameboard_offsetx,0+Gameboard_offsety, setDisplay)
 
     pygame.display.update()
     fpsTime.tick(fps)
